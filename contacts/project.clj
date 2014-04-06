5c8477184c057e10bf16cad14a0be0cbb37cc32b
David Nolen <david.nolen@gmail.com>
2014-04-06 16:24:15 -0400

    basic schema

--- contacts/project.clj
+++ contacts/project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]]
+                 [org.clojure/clojurescript "0.0-2173"]
+                 [com.datomic/datomic-free "0.9.4699"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]]
 
