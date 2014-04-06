18ff41c1b31488dd26eac5574df5983a71536e33
David Nolen <david.nolen@gmail.com>
2014-04-06 16:08:24 -0400

    use working dependencies with LT

--- contacts/project.clj
+++ contacts/project.clj
@@ -5,9 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurecript "0.0-2202"]]
+                 [org.clojure/clojurecript "0.0-2173"]]
 
-  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
 
   :cljsbuild {
     :builds [
