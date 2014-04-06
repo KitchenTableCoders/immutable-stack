5c3b32b05d50de0fd1def23ca90b71e119a3d69a
David Nolen <david.nolen@gmail.com>
2014-04-06 16:09:15 -0400

    cljs dep typo in project.clj

--- contacts/project.clj
+++ contacts/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurecript "0.0-2173"]]
+                 [org.clojure/clojurescript "0.0-2173"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]]
 
