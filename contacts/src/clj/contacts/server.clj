281e07dfae9543598fba67c6a9c9ae66dec0dd82
kovasb <kovas.boguta@gmail.com>
2014-04-06 19:57:29 -0400

    WIP

--- contacts/src/clj/contacts/server.clj
+++ contacts/src/clj/contacts/server.clj
@@ -5,40 +5,63 @@
             [ring.middleware.edn :refer [wrap-edn-params]]
             [ring.middleware.resource :refer [wrap-resource]]
             [bidi.bidi :refer [make-handler] :as bidi]
-            [com.stuartsierra.component :as component]))
+            [com.stuartsierra.component :as component]
+            [datomic.api :as d]
+            [contacts.datomic]
+            ))
 
 (def routes
   ["/" {"" :index
-        "index.html" :index}])
+        "index.html" :index
+        "contacts" :contacts}
+   ])
 
 (defn index [req]
   (file-response "public/html/index.html" {:root "resources"}))
 
+(defn generate-response [data & [status]]
+  {:status (or status 200)
+   :headers {"Content-Type" "application/edn"}
+   :body (pr-str data)}
+  )
+
+(defn contacts [req]
+  (generate-response (contacts.datomic/list-contacts (d/db (:datomic-connection req)))))
+
 (defn handler [req]
-  (println "in-handler req: " req)
+
 
   (let [match (bidi/match-route routes (:uri req))]
     (println "match: " match)
 
     (case (:handler match)
       :index (index req)
+      :contacts (let [ r (contacts req)] (println r) r)
       req)))
 
 (defn wrap-connection [handler conn]
   (fn [req] (handler (assoc req :datomic-connection conn))))
 
-(defn contacts-handler [conn]
+(defn contacts-handler* [conn]
   (wrap-resource
     (wrap-edn-params (wrap-connection handler conn))
     "public"))
 
-(defrecord WebServer [port container datomic-connection]
+(def contacts-hander [conn]
+  (fn [req]
+    ((contact-handler* conn) req)))
+
+;
+
+(defrecord WebServer [port handler container datomic-connection]
   component/Lifecycle
   (start [component]
-    (let [ handler (contacts-handler datomic-connection)
-           container (run-jetty handler {:port port :join? false})]
+    (let [ handler1   (handler datomic-connection)
+           container (run-jetty handler1 {:port port :join? false})]
       (assoc component :container container)))
   (stop [component]
     (.stop container)))
 
-(defn web-server [web-port] (WebServer. web-port nil nil))
+
+
+(defn web-server [web-port] (WebServer. web-portd contacts-handler* nil nil))
