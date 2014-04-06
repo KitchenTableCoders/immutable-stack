4e39895587532f8ec832c59cfe2b3e14490fb395
David Nolen <david.nolen@gmail.com>
2014-04-06 16:06:44 -0400

    add files

--- contacts/project.clj
+++ contacts/project.clj
@@ -3,4 +3,29 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurecript "0.0-2202"]]
+
+  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
+
+  :cljsbuild {
+    :builds [
+      {:id "dev"
+       :source-paths ["src/cljs"]
+       :compiler {
+         :output-to "resources/public/js/main.js"
+         :output-dir "resoures/public/js/out"
+         :optimizations :none
+         :source-map true
+       }}
+      {:id "release"
+       :source-paths ["src/cljs"]
+       :compiler {
+         :output-to "reources/public/js/main.js"
+         :optimizations :advanced
+         :output-wrapper true
+         :pretty-print false
+       }}
+    ]
+  })
