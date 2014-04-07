c7a11b6130c0efe07b745a8ebb26ee086bc1d33b
David Nolen <david.nolen@gmail.com>
2014-04-06 20:38:30 -0400

    can render contacts

--- contacts/src/cljs/contacts/core.cljs
+++ contacts/src/cljs/contacts/core.cljs
@@ -1,6 +1,34 @@
-(ns contacts.core)
+(ns contacts.core
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [secretary.core :as s :include-macros true :refer [defroute]]
+            [goog.dom :as gdom]
+            [contacts.util :as util]))
 
 (enable-console-print!)
 
+(def app-state (atom {}))
 
-(println "Hello world!")
+(defn contacts-view [contacts owner]
+  (reify
+    om/IRender
+    (render [_]
+      (println contacts)
+      (apply dom/ul nil
+        (map #(dom/li nil (str (:person/last-name %) ", " (:person/first-name %)))
+             contacts)))))
+
+(defn app-view [app owner]
+  (reify
+    om/IRender
+    (render [_]
+      (om/build contacts-view (:contacts app)))))
+
+(util/edn-xhr
+ {:method :get
+  :url "/contacts"
+  :on-complete
+  (fn [res]
+    (swap! app-state assoc :contacts res)
+    (om/root app-view app-state
+       {:target (gdom/getElement "contacts")}))})
---
+++ contacts/src/cljs/contacts/util.cljs
@@ -0,0 +1,21 @@
+(ns contacts.util
+  (:require [goog.events :as events]
+            [cljs.reader :as reader])
+  (:import [goog.net XhrIo]
+           goog.net.EventType
+           [goog.events EventType]))
+
+(def ^:private meths
+  {:get "GET"
+   :put "PUT"
+   :post "POST"
+   :delete "DELETE"})
+
+(defn edn-xhr [{:keys [method url data on-complete]}]
+  (let [xhr (XhrIo.)]
+    (events/listen xhr goog.net.EventType.COMPLETE
+      (fn [e]
+        (on-complete (reader/read-string (.getResponseText xhr)))))
+    (. xhr
+      (send url (meths method) (when data (pr-str data))
+        #js {"Content-Type" "application/edn"}))))
