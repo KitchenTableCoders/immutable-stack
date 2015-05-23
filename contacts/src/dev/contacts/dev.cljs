a5e9b64855e795cf674d68052842f362dc2e6ebc
David Nolen <david.nolen@gmail.com>
2015-05-23 13:37:32 -0400

    include code mirror as dep, wip

--- contacts/src/dev/contacts/dev.cljs
+++ contacts/src/dev/contacts/dev.cljs
@@ -2,15 +2,20 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [clojure.browser.repl :as repl]
             [ajax.core :refer [POST]]
-            [cljs-http.client :as http]))
+            [cljs-http.client :as http]
+            [cljs.pprint :as pprint :refer [pprint]]))
 
 (defonce conn
   (repl/connect "http://localhost:9000/repl"))
+
+(defn log [x]
+  (println) ;; flush past prompt
+  (pprint x))
 
 (comment
   (let [c (http/post "http://localhost:8081/contacts"
             {:transit-params
              {:selector [:person/last-name :person/first-name
                          {:person/telephone [:telephone/number]}]}})]
-    (go (println (:body (<! c)))))
+    (go (log (:body (<! c)))))
   )
