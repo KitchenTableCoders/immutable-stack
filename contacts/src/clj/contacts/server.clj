3772dd775526f4e69cbc1873237301ce90429c39
dnolen <dnolen@cognitect.com>
2015-05-23 15:27:28 -0400

    some cleanup

--- contacts/src/clj/contacts/server.clj
+++ contacts/src/clj/contacts/server.clj
@@ -12,7 +12,7 @@
             [contacts.datomic]))
 
 ;; =============================================================================
-;; Routing
+;; Routes
 
 (def routes
   ["" {"/" :index
@@ -36,12 +36,10 @@
   (assoc (resource-response "html/index.html" {:root "public"})
     :headers {"Content-Type" "text/html"}))
 
-
 (defn generate-response [data & [status]]
   {:status  (or status 200)
    :headers {"Content-Type" "application/transit+json"}
    :body    data})
-
 
 ;; CONTACT HANDLERS
 
@@ -52,34 +50,29 @@
         (d/db (:datomic-connection req))
         (-> req :transit-params :selector)))))
 
-
 (defn contact-get [req id]
   (generate-response
     (contacts.datomic/get-contact
       (d/db (:datomic-connection req)) id)))
-
 
 (defn contact-create [req]
   (generate-response
     (contacts.datomic/create-contact
       (:datomic-connection req)
       ;; must have form {:person/first-name "x" :person/last-name "y}
-      (:edn-params req))))
-
+      (:transit-params req))))
 
 (defn contact-update [req id]
   (generate-response
     (contacts.datomic/update-contact
       (:datomic-connection req)
-      (assoc (:edn-params req) :db/id id))))
-
+      (assoc (:transit-params req) :db/id id))))
 
 (defn contact-delete [req id]
   (generate-response
     (contacts.datomic/delete-contact
       (:datomic-connection req)
       id)))
-
 
 ;;;; PHONE HANLDERS
 
@@ -88,13 +81,13 @@
     (contacts.datomic/create-phone
       (:datomic-connection req)
       ;; must have form {:person/_telephone person-id}
-      (:edn-params req))))
+      (:transit-params req))))
 
 (defn phone-update [req id]
   (generate-response
     (contacts.datomic/update-phone
       (:datomic-connection req)
-      (assoc (:edn-params req) :db/id id))))
+      (assoc (:transit-params req) :db/id id))))
 
 (defn phone-delete [req id]
   (generate-response
@@ -121,13 +114,10 @@
       :phone-create (phone-create req)
       :phone-update (phone-update req (:id (:params match)))
       :phone-delete (phone-delete req (:id (:params match)))
-
       req)))
-
 
 (defn wrap-connection [handler conn]
   (fn [req] (handler (assoc req :datomic-connection conn))))
-
 
 (defn contacts-handler [conn]
   (wrap-resource
@@ -135,11 +125,12 @@
       (wrap-transit-params (wrap-connection handler conn)))
     "public"))
 
-
 (defn contacts-handler-dev [conn]
   (fn [req]
     ((contacts-handler conn) req)))
 
+;; =============================================================================
+;; WebServer
 
 (defrecord WebServer [port handler container datomic-connection]
   component/Lifecycle
@@ -154,8 +145,8 @@
   (stop [component]
     (.stop container)))
 
-
 (defn dev-server [web-port] (WebServer. web-port contacts-handler-dev true nil))
+
 (defn prod-server [] (WebServer. nil contacts-handler false nil))
 
 ;; =============================================================================
@@ -171,13 +162,13 @@
   ;; create contact
   (handler {:uri "/contacts"
             :request-method :post
-            :edn-params {:person/first-name "Bib" :person/last-name "Bibooo"}
+            :transit-params {:person/first-name "Bib" :person/last-name "Bibooo"}
             :datomic-connection (:connection (:db @contacts.core/servlet-system))})
 
   ;; update contact
   (handler {:uri "/contacts/17592186045434"
             :request-method :put
-            :edn-params {:person/first-name "k" :person/last-name "b"}
+            :transit-params {:person/first-name "k" :person/last-name "b"}
             :datomic-connection (:connection (:db @contacts.core/servlet-system))})
 
   ;; delete contact
@@ -188,14 +179,14 @@
   ;; create phone
   (handler {:uri "/phone"
             :request-method :post
-            :edn-params {:telephone/number "000-111-2222"
+            :transit-params {:telephone/number "000-111-2222"
                          :person/_telephone "17592186045438"}
             :datomic-connection (:connection (:db @contacts.core/servlet-system))})
 
   ;; update phone
   (handler {:uri "/phone/17592186045444"
             :request-method :put
-            :edn-params {:telephone/number "999-888-7777"}
+            :transit-params {:telephone/number "999-888-7777"}
             :datomic-connection (:connection (:db @contacts.core/servlet-system))})
 
 
