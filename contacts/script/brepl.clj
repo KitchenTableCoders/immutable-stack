cc1bf304ee3826b64171faebc890e5e6fffed1ba
David Nolen <david.nolen@gmail.com>
2015-06-06 10:29:46 -0400

    Submit -> Pull, simplify repl script

--- contacts/script/brepl.clj
+++ contacts/script/brepl.clj
@@ -2,10 +2,8 @@
 (require '[cljs.repl :as repl])
 (require '[cljs.repl.browser :as browser])
 
-(b/build (b/inputs "src/dev")
-  {:main 'contacts.dev
-   :asset-path "/js"
-   :output-to "resources/public/js/demo.js"
+(def shared-opts
+  {:asset-path "/js"
    :output-dir "resources/public/js"
    :foreign-libs
    [{:provides ["cljsjs.codemirror.addons.matchbrackets"]
@@ -15,14 +13,10 @@
      :requires ["cljsjs.codemirror"]
      :file "public/codemirror/closebrackets.js"}]})
 
-(repl/repl
-  (browser/repl-env :host-port 8081)
-  :asset-path "js"
-  :output-dir "resources/public/js"
-  :foreign-libs
-  [{:provides ["cljsjs.codemirror.addons.matchbrackets"]
-    :requires ["cljsjs.codemirror"]
-    :file "public/codemirror/matchbrackets.js"}
-   {:provides ["cljsjs.codemirror.addons.closebrackets"]
-    :requires ["cljsjs.codemirror"]
-    :file "public/codemirror/closebrackets.js"}])
+(b/build (b/inputs "src/dev")
+  (merge
+    {:main 'contacts.dev
+     :output-to "resources/public/js/demo.js"}
+    shared-opts))
+
+(repl/repl* (browser/repl-env :host-port 8081) shared-opts)
